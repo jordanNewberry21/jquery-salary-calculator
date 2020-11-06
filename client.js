bfdff595d7ecd985bfe2cc8c7c5cc1c41e06c93e
Jordan Newberry <jordan.newberry@gmail.com>
2020-11-06 16:17:29 -0600

    put in the table to append the information to the DOM

--- client.js
+++ client.js
@@ -1,7 +1,22 @@
 $(document).ready(readyNow);
 
 function readyNow() {
-    console.log('Hello from jQuery!');
+    $('#btn-add').on('click', addEmployee)
 }
 
-console.log('Hello from JS');
+function addEmployee(event) {
+    event.preventDefault();
+    console.log('Adding Employee');
+    let firstName = $('#first-name').val();
+    let lastName = $('#last-name').val();
+    let employeeID = $('#employee-id-number').val();
+    let jobTitle = $('#job-title').val();
+    let annualSalary = $('#annual-salary').val();
+    $('#first-name').val('');
+    $('#last-name').val('');
+    $('#employee-id-number').val('');
+    $('#job-title').val('');
+    $('#annual-salary').val('');
+
+    $('#employee-list').append(`<tr><td>${firstName}</td><td>${lastName}</td><td>${employeeID}</td><td>${jobTitle}</td><td>${annualSalary}</td></tr>`);
+}
